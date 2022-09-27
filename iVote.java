eb67941463b64bb94aca5228fe489ce0638654cc
vmarc <vmarcial8209@gmail.com>
2022-09-26 19:32:52 -0700

    made slight change to text for git

--- iVote.java
+++ iVote.java
@@ -2,7 +2,7 @@ public final class  iVote{
 
 public static void main( String [ ] args )
 {    
-    System.out.print("testing git testing git again becuase something is wrong  ");
+    System.out.print("testing git testing git again bething is wrong  ");
 
 
     }
