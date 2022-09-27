fe0eee637bf4684269d21fa1db61182aa4828e6c
vmarc <vmarcial8209@gmail.com>
2022-09-26 19:36:00 -0700

    testing

--- iVote.java
+++ iVote.java
@@ -2,7 +2,7 @@ public final class  iVote{
 
 public static void main( String [ ] args )
 {    
-    System.out.print("testing git testing git again bething is wrong  ");
+    System.out.print("testing git ehy isnt this working i swear testing git again bething is wrong  ");
 
 
     }
